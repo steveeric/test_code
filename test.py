46c54792a2d5847ab8365eccc34231c72785e368
SHOTA <ohennjiwo@yahoo.co.jp>
2014-08-28 15:54:25 +0900

    新規テストコミットです.
    このファイルは私が初めてpythonで作成したコードです.

---
+++ test.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import sys
+print u'uso'
+print(sys.version_info.major)
+print(sys.version_info.minor)
+print(sys.version_info.micro)
+
+major = sys.version_info.major
+minor = sys.version_info.minor
+micro = sys.version_info.micro
+
+print str(major)+"."+str(minor)+"."+str(micro)
+
+for num in [4, 3, 12]:
+  print "num = " + str(num)
+print "End"
+
+print u'owari';
